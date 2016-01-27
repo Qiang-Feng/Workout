9eb7a3c643dd5a1edea108539b3c1df85a28c06a
Qiang-Feng <Qiang.Feng@hotmail.co.uk>
2016-01-27 22:45:23 +0000

    Removed menu from BaseActivity

--- app/src/main/java/com/qiang/workout/BaseActivity.java
+++ app/src/main/java/com/qiang/workout/BaseActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -139,14 +138,6 @@ public class BaseActivity extends AppCompatActivity
 	{
 		super.onConfigurationChanged(newConfig);
 		drawerToggle.onConfigurationChanged(newConfig);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present
-		getMenuInflater().inflate(R.menu.menu_base, menu);
-		return true;
 	}
 
 	@Override
