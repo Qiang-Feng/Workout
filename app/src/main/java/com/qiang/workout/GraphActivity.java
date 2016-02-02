607ab52324931b8e03ba114fef0efb8f7a5ce88d
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2016-02-02 12:22:55 +0000

    GraphActivity: added comments

--- app/src/main/java/com/qiang/workout/GraphActivity.java
+++ app/src/main/java/com/qiang/workout/GraphActivity.java
@@ -43,26 +43,48 @@ public class GraphActivity extends AppCompatActivity
 		entries = new ArrayList<>();
 		dateAxisLabels = new ArrayList<>();
 
-		// Customise the bar chart
+		// Removes the default text from the BarChart
 		chart.setDescription("");
 		chart.setNoDataTextDescription("");
 		chart.setNoDataText("");
-		chart.setPinchZoom(false);
-		chart.setDoubleTapToZoomEnabled(false);
-		chart.getAxisRight().setEnabled(false);
-		chart.getXAxis().setDrawGridLines(false);
-		chart.setDrawGridBackground(false);
-		chart.getLegend().setEnabled(false);
-		chart.getXAxis().setAxisLineWidth(3F);
-		chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+		// Set the font size for text inside the BarChart
 		chart.getAxisLeft().setTextSize(13F);
 		chart.getXAxis().setTextSize(13F);
+
+		chart.setPinchZoom(false);
+		chart.setDoubleTapToZoomEnabled(false);
+
+		// Removes right Y axis
+		chart.getAxisRight().setEnabled(false);
+
+		// Removes left Y axis line
+		chart.getAxisLeft().setDrawAxisLine(false);
+
+		// Removes grid lines
+		chart.getXAxis().setDrawGridLines(false);
+		chart.setDrawGridBackground(false);
+
+		// Disables the legend
+		chart.getLegend().setEnabled(false);
+
+		// Make the X axis thicker
+		chart.getXAxis().setAxisLineWidth(3F);
+
+		// Place the X axis at the bottom of the screen
+		chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+		// Disables highlighting of the bars
 		chart.setHighlightPerDragEnabled(false);
 		chart.setHighlightPerTapEnabled(false);
+
+		// Prevents first and last bars from being "clipped" off the screen
 		chart.getXAxis().setAvoidFirstLastClipping(true);
-		chart.getAxisLeft().setDrawAxisLine(false);
+
+		// Sets the colour for the Y axis grid lines
 		chart.getAxisLeft().setGridColor(Color.rgb(104, 241, 175));
 
+		// Formats the times text on the Y axis
 		chart.getAxisLeft().setValueFormatter(new YAxisValueFormatter()
 		{
 			@Override
@@ -72,18 +94,25 @@ public class GraphActivity extends AppCompatActivity
 			}
 		});
 
+		// Retrieves the times from the database
 		List<StopwatchTime> times = dbHandler.allStopwatchTimes(dbHandler.getCategory(getIntent().getIntExtra("stopwatchCategoryID", 0)));
 
+		// Adds all times to the "entries" variable
 		for (int i = 0; i < times.size(); i++)
 		{
 			entries.add(new BarEntry(times.get(i).getTime(), i));
+
+			// Formats the dates to be displayed on the X axis
 			dateAxisLabels.add(new SimpleDateFormat("dd/MM/yy").format(times.get(i).getRecordDate() * 1000L));
 		}
 
 		BarDataSet dataSet = new BarDataSet(entries, "");
+
+		// Sets the bar colour and the text size for the time displayed at top
 		dataSet.setColor(Color.rgb(242, 247, 158));
 		dataSet.setValueTextSize(13F);
 
+		// Formats the time for each bar
 		dataSet.setValueFormatter(new ValueFormatter()
 		{
 			@Override
@@ -93,6 +122,7 @@ public class GraphActivity extends AppCompatActivity
 			}
 		});
 
+		// Puts retried data into the BarChart
 		chart.setData(new BarData(dateAxisLabels, dataSet));
 
 		// Refreshes the chart
